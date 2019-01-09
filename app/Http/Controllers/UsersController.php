9a09eb5a34cbb21abeaa333a12c6f352922cc55b
yueguang <yueguang@qingmutec.com>
2019-01-09 18:16:29 +0800

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,7 +12,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => [ 'create', 'store']
+            'except' => [ 'create', 'store', 'index']
         ]);
 
         $this->middleware('guest', [
@@ -79,6 +79,14 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
 
         return redirect()->route('users.show', $user);
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 
 }
