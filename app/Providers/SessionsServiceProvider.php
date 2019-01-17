421d4592ae75bf61d43acfecc3b4c35dfbb098a4
yueguang <yueguang@qingmutec.com>
2019-01-17 20:31:18 +0800

    容器类-接口类-业务类添加，实现了开放封闭原则，Controller只需要引入接口，业务类通过服务类绑定

--- app/Providers/SessionsServiceProvider.php
+++ app/Providers/SessionsServiceProvider.php
@@ -2,28 +2,27 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+class SessionsServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap any application services.
+     * Bootstrap services.
      *
      * @return void
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        //
     }
 
     /**
-     * Register any application services.
+     * Register services.
      *
      * @return void
      */
     public function register()
     {
-
+        $this->app->bind('App\Interfaces\SessionsServerInterface', 'App\Server\SessionsServer');
     }
 }
